95f9fb38dff7730215fc7bd785dc5b4d7e482c7a
Jeremy Jacobson <29494813+xxk-i@users.noreply.github.com>
2023-04-14 20:48:02 -0400

    More things

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,4 +1,4 @@
-use std::{path::{PathBuf, self}, fs};
+use std::{path::{PathBuf}, fs, string};
 
 use actix_web::{Responder, HttpResponse};
 
@@ -6,6 +6,7 @@ use self::errors::*;
 
 mod errors;
 
+#[derive(Clone)]
 pub struct Database {
     pub path: PathBuf,
     world_count: u64,
@@ -14,18 +15,82 @@ pub struct Database {
 
 impl Database {
     pub fn new(path: PathBuf) -> Result<Self, DatabaseError> {
-        let mut db_path = path.clone();
-        db_path.push("db");
-
-        if !db_path.exists() {
-            fs::create_dir(db_path)?;
+        if !path.exists() {
+            fs::create_dir(&path)?;
         }
 
-        Ok(Database { path: path, world_count: 0, current_world: 0 })
+        let mut db = Database { path: path, world_count: 0, current_world: 0 };
+
+        db.initalize_db_directory()?;
+
+        Ok(db)
+    }
+
+    pub fn from(path: PathBuf) -> Result<Self, DatabaseError> {
+        let mut db = Database { path: path, world_count: 0, current_world: 0 };
+
+        db.world_count = db.find_latest_world()?;
+        db.switch_world(db.world_count);
+
+        Ok(db)
+    }
+
+    fn initalize_db_directory(&mut self) -> Result<(), DatabaseError> {
+        // error if our new directory is not empty
+        if !self.path.read_dir()?.next().is_none() {
+            return Err(DatabaseError)
+        }
+
+        fs::create_dir_all(self.path.clone().join("worlds/world1"))?;
+
+        self.world_count = 1;
+        self.switch_world(1).expect("Failed to switch to first world in new database");
+
+        Ok(())
+    }
+
+    fn find_latest_world(&mut self) -> Result<u64, DatabaseError> {
+        let mut highest = 0u64;
+
+        for dir in self.path.join("worlds").read_dir()? {
+            let dir = dir?;
+            if dir.file_type()?.is_dir() {
+                let filename = dir.file_name();
+                let split: Vec<&str> = filename.to_str().unwrap().split("world").collect();
+                let last = split.last().unwrap();
+                
+                let world_number = last.parse::<u64>().unwrap();
+                if world_number > highest {
+                    highest = world_number;
+                }
+            }
+        }
+
+        // self.world_count = highest;
+        // self.switch_world(highest).expect("Failed to switch to latest found world");
+
+        Ok(highest)
     }
 
     pub fn update_time_in_water(&mut self, uuid: String, time: u64) {
         unimplemented!()
+    }
+
+    pub fn world_death_event(&mut self) {
+        self.switch_world(self.world_count);
+    }
+
+    pub fn create_world(&mut self) -> Result<(), DatabaseError> {
+        self.world_count += 1;
+        let string_numerical_suffix = self.world_count.to_string();
+        let mut path = "worlds/world".to_owned();
+        path.push_str(&string_numerical_suffix);
+
+        fs::create_dir_all(self.path.join(path))?;
+
+        self.switch_world(self.world_count).unwrap();
+
+        Ok(())
     }
 
     pub fn get_path(&self) -> impl Responder {
@@ -36,24 +101,15 @@ impl Database {
         HttpResponse::Ok().body(self.world_count.to_string())
     }
 
-    // pub fn switch_world(&mut self, world: u64) -> Result<(), WorldNotFoundError> {
-    //     if world > self.world_count {
-    //         return Err(errors::WorldNotFoundError {
-    //             world_number: world
-    //         })
-    //     }
-
-    //     self.current_world = world;
-    //     Ok(())
-    // }
-
-    pub fn switch_world(&self, world: u64) -> Result<(), WorldNotFoundError> {
-        if world > self.world_count {
+    pub fn switch_world(&mut self, world: u64) -> Result<(), WorldNotFoundError> {
+        if world == 0 || world > self.world_count {
             return Err(errors::WorldNotFoundError {
                 world_number: world
             })
         }
 
+        self.current_world = world;
         Ok(())
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, put};
-use serde::Deserialize;
-use std::sync::Mutex;
+use serde::{Deserialize, de::Expected};
+use std::{sync::Mutex, fs};
 
 mod db;
 use db::Database;
 
-pub struct APIData {
-    pub database: Mutex<Database>
+pub struct APIData<'a> {
+    pub database: Mutex<&'a mut Database>,
+    auth: String,
 }
 
 #[derive(Deserialize)]
@@ -18,6 +19,7 @@ struct SwitchInfo {
 #[derive(Deserialize)]
 #[allow(non_snake_case)] // we use json naming convention here
 struct Info {
+    auth: String,
     UUID: String,
     timeInWater: Option<u64>,
     damageTaken: Option<u64>,
@@ -49,10 +51,13 @@ pub async fn get_db_path(data: web::Data<APIData>) -> impl Responder {
 async fn stats(data: web::Data<APIData>, info: web::Json<Info>) -> impl Responder {
     let _db = data.database.lock().unwrap();
 
+    if !info.auth.eq(&data.auth) {
+        return HttpResponse::Forbidden()
+    }
+
     if let Some(time) = &info.timeInWater {
         println!("{}'s time spent in water: {}", info.UUID, time);
     }
-
 
     if let Some(damage_taken)  = &info.damageTaken {
         println!("{}'s damage taken: {}", info.UUID, damage_taken);
@@ -79,7 +84,7 @@ async fn stats(data: web::Data<APIData>, info: web::Json<Info>) -> impl Responde
 
 #[put("/world")]
 async fn switch_world(data: web::Data<APIData>, switch_info: web::Json<SwitchInfo>) -> impl Responder {
-    let db = data.database.lock().unwrap();
+    let mut db = data.database.lock().unwrap();
 
     return match db.switch_world(switch_info.world) {
         Ok(()) => {
@@ -92,17 +97,53 @@ async fn switch_world(data: web::Data<APIData>, switch_info: web::Json<SwitchInf
     }
 }
 
+#[put("/world/create")]
+async fn create_world(data: web::Data<APIData>) -> impl Responder {
+    let mut db = data.database.lock().unwrap();
+
+    return match db.create_world() {
+        Ok(()) => {
+            HttpResponse::Ok().body("OK")
+        },
+
+        Err(e) => {
+            HttpResponse::BadRequest().body(e.to_string())
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let path = std::env::current_dir().unwrap().join("db");
+    let mut db = match path.exists() {
+        true => {
+            println!("Retrieving existing database");
+            Database::from(path).expect("Something went wrong getting the existing database")
+        },
+        false => {
+            println!("Creating new database");
+            Database::new(path).expect("Something went wrong creating a new database")
+        },
+    };
+
+    // HttpServer server spawns "workers" equal to the number
+    // of phyiscal cpu's / cores available in the system
+    //
+    // the db is created outside of HttpServer::new so it is only created once,
+    // we have to clone the db into each HttpServer
+    HttpServer::new(move || {
+
         App::new()
         .app_data(web::Data::new(APIData {
-            database: Mutex::new(Database::new(std::env::current_dir().unwrap()).expect("Something went wrong creating the database"))
+            // database: Mutex::new(db.clone()),
+            database: Mutex::new(&mut db),
+            auth: fs::read_to_string("auth.txt").expect("Auth setup failed")
         }))
             .service(hello)
             .service(stats)
             .service(get_current_world)
             .service(get_db_path)
+            .service(create_world)
             .service(switch_world)
     })
     .bind(("127.0.0.1", 8080))?
